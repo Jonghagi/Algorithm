599a4a90e22321787b5c51b5ef60a549aa41b9b3
Jonghagi <jonghagi77@gmail.com>
2016-04-29 14:18:35 +0900

    Change data type because input range is too big

--- PinaryNumber/src/Main.java
+++ PinaryNumber/src/Main.java
@@ -4,22 +4,24 @@ import java.util.Scanner;
  * Created by Kimjonghak on 2016. 4. 29..
  */
 public class Main {
-    public static int Fibonnaci(int n){
-        if(n == 1){
-            return 1;
-        } else if(n == 2){
-            return 1;
-        } else {
-            return Fibonnaci(n-1) + Fibonnaci(n-2);
-        }
-    }
 
     public static void main(String args[]){
         Scanner input;
         int n;
+        long[] numberOfPinary;
         input = new Scanner(System.in);
         n = input.nextInt();
+        numberOfPinary = new long[n];
 
-        System.out.println(Fibonnaci(n));
+        for(int i = 0; i < n; i++){
+            if(i == 0){
+                numberOfPinary[i] = 1;
+            } else if(i == 1){
+                numberOfPinary[i] = 1;
+            } else {
+                numberOfPinary[i] = numberOfPinary[i-1] + numberOfPinary[i-2];
+            }
+        }
+        System.out.println(numberOfPinary[n-1]);
     }
 }
